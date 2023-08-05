6c36ce404a18047a02758fed89572d28b4d57d1d
mrsiri <vimavick@gmail.com>
2023-08-05 13:38:28 -0300

    Revert "common.mk: Update RIL dependencies"
    This reverts commit c7856bf0e7e6c142d3a5cd820e94d4cf3ce4ba5e.

--- common.mk
+++ common.mk
@@ -380,12 +380,18 @@ PRODUCT_COPY_FILES += \
 # RIL
 PRODUCT_PACKAGES += \
     android.hardware.radio@1.5.vendor \
+    android.hardware.radio.config@1.1.vendor \
     android.hardware.radio.config@1.2.vendor \
     android.hardware.radio.deprecated@1.0.vendor \
     libxml2 \
     librilutils \
     librmnetctl \
-    secril_config_svc
+    secril_config_svc \
+    libsec-ril \
+    libsecril-client \
+    libsecril-client-sap \
+    modemloader \
+    SamsungServiceMode
     
 # IPv6
 PRODUCT_PACKAGES += \
