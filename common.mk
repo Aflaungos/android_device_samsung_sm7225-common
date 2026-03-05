b120c28d7f2425f622234205a35f3c733e2c4abb
Aflaungos <vimavick@gmail.com>
2026-03-04 21:10:16 -0300

    sm7225-common: Remove soundtrigger @2.2
    * We don't need it if we're building @2.3 already.

--- common.mk
+++ common.mk
@@ -89,7 +89,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.service \
     android.hardware.audio@7.0-impl.samsung-sm7225 \
     android.hardware.audio.effect@7.0-impl \
-    android.hardware.soundtrigger@2.2-impl \
     android.hardware.soundtrigger@2.3-impl \
     sound_trigger.primary.lito \
     audio.bluetooth.default \
