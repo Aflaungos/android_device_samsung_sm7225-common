58b8d37253cd871613021bb5500797568ede4c46
LuK1337 <priv.luk@gmail.com>
2026-03-05 00:20:03 -0300

    sm7225-common: Remove vendor/lineage device framework matrix inclusion

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -192,11 +192,10 @@ TARGET_BOARD_PLATFORM_GPU := qcom-adreno619
 # HIDL Manifests
 DEVICE_MANIFEST_FILE := $(COMMON_PATH)/configs/manifest.xml
 
-DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE := \
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
     hardware/qcom-caf/common/vendor_framework_compatibility_matrix_legacy.xml \
-    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml \
-    vendor/lineage/config/device_framework_matrix.xml
+    hardware/samsung/vintf/samsung_framework_compatibility_matrix.xml
 
 DEVICE_MATRIX_FILE := hardware/qcom-caf/common/compatibility_matrix.xml
 
