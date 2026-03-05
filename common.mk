6466a000d31616e4afd1bf3173fee7ad09bafe30
me-cafebabe <me.cafebabe@gmail.com>
2026-03-04 21:27:37 -0300

    sm7225-common: Update some soong config variables to bool type

--- common.mk
+++ common.mk
@@ -242,7 +242,7 @@ PRODUCT_PACKAGES += \
 $(call soong_config_set,lineage_health,charging_control_charging_path,/sys/class/power_supply/battery/batt_slate_mode)
 $(call soong_config_set,lineage_health,charging_control_charging_enabled,0)
 $(call soong_config_set,lineage_health,charging_control_charging_disabled,1)
-$(call soong_config_set,lineage_health,charging_control_supports_bypass,false)
+$(call soong_config_set_bool,lineage_health,charging_control_supports_bypass,false)
 
 # HIDL
 PRODUCT_PACKAGES += \
@@ -508,7 +508,7 @@ PRODUCT_PACKAGES += \
     android.hardware.vibrator-service.samsung
 
 # Vibrator
-$(call soong_config_set,samsungVibratorVars,duration_amplitude,true)
+$(call soong_config_set_bool,samsungVibratorVars,duration_amplitude,true)
 
 # Wifi
 PRODUCT_PACKAGES += \
